833f937abb84fc259b093f4a30733625b98b68db
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-08-18 16:16:17 +0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# furniture_store_application
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/furniture_store_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.furniture_store_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
