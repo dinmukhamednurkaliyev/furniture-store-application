03a04f221fe1eb0eae2876632b89a6f92adbc872
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-09-02 16:18:02 +0500

    refactor(authentication): refine sign-in page UI and code quality

--- lib/features/authentication/pages/sign_in_page.dart
+++ lib/features/authentication/pages/sign_in_page.dart
@@ -1,148 +1,155 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:furniture_store_application/core/core.dart';
+import 'package:furniture_store_application/features/authentication/authentication.dart';
 
 class SignInPage extends ConsumerStatefulWidget {
   const SignInPage({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _SignInPageState();
+  ConsumerState<SignInPage> createState() => _SignInPageState();
 }
 
 class _SignInPageState extends ConsumerState<SignInPage> {
-  final _formKey = GlobalKey<FormState>();
-  final _emailController = TextEditingController();
-  final _passwordController = TextEditingController();
+  final GlobalKey<FormState> signInFormGlobalKey = GlobalKey<FormState>();
+  final TextEditingController emailTextEditingController =
+      TextEditingController();
+  final TextEditingController passwordTextEditingController =
+      TextEditingController();
 
   @override
   void dispose() {
-    _emailController.dispose();
-    _passwordController.dispose();
+    emailTextEditingController.dispose();
+    passwordTextEditingController.dispose();
     super.dispose();
   }
 
-  Future<void> _signIn() async {}
+  Future<void> handleSignInAttempt() async {
+    if (signInFormGlobalKey.currentState!.validate()) {}
+  }
 
   @override
   Widget build(BuildContext context) {
+    final currentAuthenticationState = ref.watch(
+      authenticationNotifierProvider,
+    );
+
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsetsGeometry.all(24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 40,
-                ),
-                Text(
-                  'Welcome Back!',
-                  style: context.headlineLarge,
-                ),
-                const SizedBox(
-                  height: 4,
-                ),
-                Text(
-                  'Sign in to continue shopping',
-                  style: context.bodyLarge?.copyWith(
-                    color: Colors.grey.shade600,
-                  ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                Form(
-                  key: _formKey,
-                  child: Column(
-                    children: [
-                      AuthenticationTextFieldWidget(
-                        prefixIcon: const Icon(Icons.email),
-                        label: 'Email',
-                        hintText: 'Emter your email',
-                        controller: _emailController,
-                        keyboardType: TextInputType.emailAddress,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your email';
-                          }
-                          if (!value.contains('@')) {
-                            return 'Please enter a valid email';
-                          }
-                          return null;
-                        },
+          child: Center(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(
+                maxWidth: 600,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(24),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Text(
+                      'Welcome Back!',
+                      style: context.headlineLarge,
+                    ),
+                    const SizedBox(
+                      height: 4,
+                    ),
+                    Text(
+                      'Sign in to continue shopping',
+                      style: context.bodyLarge?.copyWith(
+                        color: Colors.grey.shade600,
                       ),
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      AuthenticationTextFieldWidget(
-                        prefixIcon: const Icon(Icons.lock),
-                        label: 'Password',
-                        hintText: 'Emter your password',
-                        controller: _passwordController,
-                        isPassword: true,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter your email';
-                          }
-                          if (value.length < 8) {
-                            return 'Password must be at least 8 characters';
-                          }
-                          return null;
-                        },
-                      ),
-                      const SizedBox(
-                        height: 12,
-                      ),
-                      Align(
-                        alignment: Alignment.centerRight,
-                        child: TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            'Forgot Password ?',
-                            style: TextStyle(
-                              color: Colors.grey.shade700,
-                              fontWeight: FontWeight.w500,
+                    ),
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Form(
+                      key: signInFormGlobalKey,
+                      child: Column(
+                        children: [
+                          AuthenticationTextFieldWidget(
+                            prefixIconWidget: const Icon(
+                              Icons.email,
+                            ),
+                            labelText: 'Email',
+                            hintTextInputText: 'Enter your email',
+                            textEditingController: emailTextEditingController,
+                            keyboardInputType: TextInputType.emailAddress,
+                            validator: (String? value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your email';
+                              }
+                              if (!value.contains('@')) {
+                                return 'Please enter a valid email';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          AuthenticationTextFieldWidget(
+                            prefixIconWidget: const Icon(Icons.lock),
+                            labelText: 'Password',
+                            hintTextInputText: 'Enter your password',
+                            textEditingController:
+                                passwordTextEditingController,
+                            isPasswordInput: true,
+                            validator: (String? value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Please enter your password';
+                              }
+                              if (value.length < 8) {
+                                return 'Password must be at least 8 characters';
+                              }
+                              return null;
+                            },
+                          ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          Align(
+                            alignment: Alignment.centerRight,
+                            child: TextButton(
+                              onPressed: () {},
+                              child: Text(
+                                'Forgot Password ?',
+                                style: TextStyle(
+                                  color: Colors.grey.shade700,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 12,
-                      ),
-                      Consumer(
-                        builder: (context, authentication, child) {
-                          if (authentication.errorMessage != null) {
-                            return const Padding(
-                              padding: EdgeInsetsGeometry.only(
-                                bottom: 16,
-                              ),
+                          const SizedBox(
+                            height: 12,
+                          ),
+                          if (currentAuthenticationState.errorMessage != null)
+                            Padding(
+                              padding: const EdgeInsets.only(bottom: 16),
                               child: Text(
-                                authentication.errorMessage,
-                                style: TextStyle(
+                                currentAuthenticationState.errorMessage!,
+                                style: const TextStyle(
                                   color: Colors.red,
                                   fontSize: 14,
                                 ),
                               ),
-                            );
-                          }
-                          return const SizedBox.shrink();
-                        },
-                      ),
-
-                      Consumer(
-                        builder: (context, authentication, child) {
+                            ),
                           AuthenticationButtonWidget(
-                            text: 'Sign In',
-                            onPressed: _signIn,
-                            isLoading: authentication.isLoading,
-                          );
-                        },
+                            buttonText: 'Sign In',
+                            onPressCallback: handleSignInAttempt,
+                            isButtonLoading:
+                                currentAuthenticationState.isLoading ?? false,
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-              ],
+              ),
             ),
           ),
         ),
@@ -153,31 +160,31 @@ class _SignInPageState extends ConsumerState<SignInPage> {
 
 class AuthenticationButtonWidget extends StatelessWidget {
   const AuthenticationButtonWidget({
-    required this.onPressed,
-    required this.text,
-    this.isLoading = false,
+    required this.onPressCallback,
+    required this.buttonText,
+    this.isButtonLoading = false,
     super.key,
   });
-  final String text;
-  final VoidCallback onPressed;
-  final bool isLoading;
+  final String buttonText;
+  final VoidCallback onPressCallback;
+  final bool isButtonLoading;
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       width: double.infinity,
       child: ElevatedButton(
-        onPressed: isLoading ? null : onPressed,
+        onPressed: isButtonLoading ? null : onPressCallback,
         style: ElevatedButton.styleFrom(
           backgroundColor: context.primaryColor,
           foregroundColor: Colors.white,
           padding: const EdgeInsets.symmetric(vertical: 16),
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadiusGeometry.circular(12),
+            borderRadius: BorderRadius.circular(12),
           ),
           elevation: 0,
         ),
-        child: isLoading
+        child: isButtonLoading
             ? const SizedBox(
                 height: 20,
                 width: 20,
@@ -187,7 +194,7 @@ class AuthenticationButtonWidget extends StatelessWidget {
                 ),
               )
             : Text(
-                text,
+                buttonText,
                 style: const TextStyle(
                   fontSize: 16,
                   fontWeight: FontWeight.w600,
@@ -211,7 +218,7 @@ class AuthenticationDividerWidget extends StatelessWidget {
           ),
         ),
         Padding(
-          padding: const EdgeInsetsGeometry.symmetric(horizontal: 16),
+          padding: const EdgeInsets.symmetric(horizontal: 16),
           child: Text(
             ' Or continue with',
             style: TextStyle(
@@ -232,19 +239,19 @@ class AuthenticationDividerWidget extends StatelessWidget {
 
 class AuthenticationSocialButtonWidget extends StatelessWidget {
   const AuthenticationSocialButtonWidget({
-    required this.icon,
-    required this.onPressed,
-    required this.text,
+    required this.iconAssetPath,
+    required this.onPressCallback,
+    required this.buttonText,
     super.key,
   });
-  final String text;
-  final String icon;
-  final VoidCallback onPressed;
+  final String buttonText;
+  final String iconAssetPath;
+  final VoidCallback onPressCallback;
 
   @override
   Widget build(BuildContext context) {
     return OutlinedButton(
-      onPressed: onPressed,
+      onPressed: onPressCallback,
       style: OutlinedButton.styleFrom(
         padding: const EdgeInsets.symmetric(vertical: 16),
         side: BorderSide(color: Colors.grey.shade300),
@@ -255,16 +262,15 @@ class AuthenticationSocialButtonWidget extends StatelessWidget {
       child: Row(
         children: [
           Image.asset(
-            icon,
+            iconAssetPath,
             height: 24,
             width: 24,
           ),
           const SizedBox(
             width: 12,
           ),
-
           Text(
-            text,
+            buttonText,
             style: context.bodyMedium?.copyWith(
               color: Colors.grey.shade800,
               fontWeight: FontWeight.w500,
@@ -278,33 +284,34 @@ class AuthenticationSocialButtonWidget extends StatelessWidget {
 
 class AuthenticationTextFieldWidget extends StatelessWidget {
   const AuthenticationTextFieldWidget({
-    required this.controller,
-    required this.hintText,
-    required this.label,
-    this.textInputAction = TextInputAction.none,
-    this.isEnable = true,
-    this.keyboardType = TextInputType.text,
-    this.isPassword = false,
+    required this.textEditingController,
+    required this.hintTextInputText,
+    required this.labelText,
+    this.textInputActionListener = TextInputAction.none,
+    this.isEnabledInput = true,
+    this.keyboardInputType = TextInputType.text,
+    this.isPasswordInput = false,
     super.key,
     this.validator,
-    this.prefixIcon,
+    this.prefixIconWidget,
   });
-  final String label;
-  final String hintText;
-  final bool isPassword;
-  final TextEditingController controller;
+  final String labelText;
+  final String hintTextInputText;
+  final bool isPasswordInput;
+  final TextEditingController textEditingController;
   final String? Function(String?)? validator;
-  final TextInputType keyboardType;
-  final TextInputAction textInputAction;
-  final bool isEnable;
-  final Widget? prefixIcon;
+  final TextInputType keyboardInputType;
+  final TextInputAction textInputActionListener;
+  final bool isEnabledInput;
+  final Widget? prefixIconWidget;
 
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          label,
+          labelText,
           style: context.bodyLarge?.copyWith(
             color: Colors.grey.shade700,
             fontWeight: FontWeight.w400,
@@ -314,21 +321,23 @@ class AuthenticationTextFieldWidget extends StatelessWidget {
           height: 8,
         ),
         TextFormField(
-          controller: controller,
-          obscureText: isPassword,
+          controller: textEditingController,
+          obscureText: isPasswordInput,
           validator: validator,
-          keyboardType: keyboardType,
-          enabled: isEnable,
-          textInputAction: textInputAction,
+          keyboardType: keyboardInputType,
+          enabled: isEnabledInput,
+          textInputAction: textInputActionListener,
           style: context.bodyLarge,
           decoration: InputDecoration(
-            prefixIcon: prefixIcon,
-            hintText: hintText,
+            prefixIcon: prefixIconWidget,
+            hintText: hintTextInputText,
             hintStyle: context.bodyLarge?.copyWith(
               color: Colors.grey.shade400,
             ),
             filled: true,
-            fillColor: isEnable ? Colors.grey.shade100 : Colors.grey.shade50,
+            fillColor: isEnabledInput
+                ? Colors.grey.shade100
+                : Colors.grey.shade50,
             border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(12),
               borderSide: BorderSide.none,
