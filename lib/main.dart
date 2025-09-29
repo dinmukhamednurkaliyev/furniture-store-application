40a9f6a4000d0b084af8c1341c4771ab12dd1ed2
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-09-29 17:35:46 +0500

    revert "refactor(bootstrap): enable asynchronous application initialization"
    This reverts commit 2dc5a8793c4ef63d247ca13cd8956cff11b1e619.

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:furniture_store_application/core/core.dart';
 import 'package:furniture_store_application/features/features.dart';
 
-Future<void> main() async {
-  await ApplicationBootstrap.run(
+void main() {
+  ApplicationBootstrap.run(
     applicationBuilder: () =>
         const ProviderScope(child: ApplicationRootWidget()),
   );
